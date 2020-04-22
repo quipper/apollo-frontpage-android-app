bac6cc1792ceef5f25298856280f2e2bef447250
padobariya <piyu.dobariya@gmail.com>
2020-04-22 18:41:16 +0900

    :sparkles: Added error toast
    :sparkles: Added error toast

--- app/src/main/java/com/quipper/android/apollofrontpage/PostListFragment.kt
+++ app/src/main/java/com/quipper/android/apollofrontpage/PostListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -37,6 +38,15 @@ class PostListFragment : Fragment(), PostListAdapter.PostListHandler {
         viewModel.postResult.posts.observe(viewLifecycleOwner, Observer {
             it ?: return@Observer
             postListAdapter.submitList(it)
+        })
+
+        viewModel.postResult.error.observe(viewLifecycleOwner, Observer {
+            it ?: return@Observer
+            Toast.makeText(
+                requireContext(),
+                resources.getString(R.string.common_error),
+                Toast.LENGTH_SHORT
+            ).show()
         })
     }
 
