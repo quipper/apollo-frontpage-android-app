3a60ecc2a081d149a99a6b0371c1ecf509ad934b
padobariya <piyu.dobariya@gmail.com>
2020-04-24 11:08:58 +0900

    :recycle: Removed fully qualified package names
    :recycle: Removed fully qualified package names

--- app/src/main/java/com/quipper/android/apollofrontpage/di/Modules.kt
+++ app/src/main/java/com/quipper/android/apollofrontpage/di/Modules.kt
@@ -2,15 +2,17 @@ package com.quipper.android.apollofrontpage.di
 
 import com.apollographql.apollo.ApolloClient
 import com.quipper.android.apollofrontpage.PostListViewModel
+import com.quipper.android.apollofrontpage.repository.PostsRepository
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import com.quipper.android.apollofrontpage.repository.impl.PostsRepository as PostsRepositoryImpl
 
 val apiModule = module {
     single { ApolloClient.builder().serverUrl("http://10.0.2.2:8080/graphql").build() }
 }
 
 val repositoryModule = module {
-    factory<com.quipper.android.apollofrontpage.repository.PostsRepository> { com.quipper.android.apollofrontpage.repository.impl.PostsRepository(get()) }
+    factory<PostsRepository> { PostsRepositoryImpl(get()) }
 }
 
 val viewModelModule = module {
